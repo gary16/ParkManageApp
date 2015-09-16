9fdfd47b5fbdb9f684cf370179f555f90fd1315b
gary16 <gary16@126.com>
2015-09-17 00:18:51 +0800

    g

--- src/com/zoway/parkmanage/view/MainActivity.java
+++ src/com/zoway/parkmanage/view/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends Activity {
 	private MainListAdapter madapter;
 	private Button malogout;
 	private TextView matxtlgtime;
-
+//
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
