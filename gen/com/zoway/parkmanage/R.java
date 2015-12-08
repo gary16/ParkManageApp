84e979f56d103d51b74ebc36878df277f6460044
gary16 <gary16@126.com>
2015-12-08 08:32:56 +0800

    20151208

--- gen/com/zoway/parkmanage/R.java
+++ gen/com/zoway/parkmanage/R.java
@@ -45,12 +45,21 @@ public final class R {
         public static final int mainlistline=0x7f02000e;
         public static final int navigatedown=0x7f02000f;
         public static final int navigateright=0x7f020010;
-        public static final int park=0x7f020011;
-        public static final int runfee=0x7f020012;
-        public static final int settings=0x7f020013;
-        public static final int takephoto=0x7f020014;
-        public static final int takephotobutton=0x7f020015;
-        public static final int userbig=0x7f020016;
+        public static final int new1=0x7f020011;
+        public static final int new2=0x7f020012;
+        public static final int new3=0x7f020013;
+        public static final int new4=0x7f020014;
+        public static final int new5=0x7f020015;
+        public static final int new6=0x7f020016;
+        public static final int new7=0x7f020017;
+        public static final int new8=0x7f020018;
+        public static final int park=0x7f020019;
+        public static final int querylist=0x7f02001a;
+        public static final int runfee=0x7f02001b;
+        public static final int settings=0x7f02001c;
+        public static final int takephoto=0x7f02001d;
+        public static final int takephotobutton=0x7f02001e;
+        public static final int userbig=0x7f02001f;
     }
     public static final class id {
         public static final int action_settings=0x7f0b0051;
